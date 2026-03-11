33d05210df5718429a609f30c909e73e3adb677c
ordinary-ryan <ryanl422@nycstudents.net>
2026-03-11 10:17:05 -0400

    Create testing.py

---
+++ testing.py
@@ -0,0 +1,27 @@
+
+def slot(quart_count,mac1,mac2,mac3):
+    play_count = 0
+    while quart_count > 0:
+        mac1 +=1
+        quart_count-= 1
+        play_count += 1
+        if mac1 == 35:
+            mac1 = 0
+            quart_count += 30
+    if quart_count > 0:
+        mac2 += 1
+        quart_count -=1
+        play_count += 1
+        if mac2 == 100:
+            mac2 = 0
+            quart_count += 60
+    if quart_count > 0:
+        mac3 += 1
+        quart_count -=1
+        play_count += 1
+        if mac3 == 10:
+            mac3 = 0
+            quart_count += 9
+    
+    print(play_count)
+slot(48,3,10,4)
