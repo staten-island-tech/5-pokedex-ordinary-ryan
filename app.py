145ee762ca3a803c444cbd71d33939e8fb213f94
ordinary-ryan <ryanl422@nycstudents.net>
2026-03-17 10:17:09 -0400

    Update app.py

--- app.py
+++ app.py
@@ -27,18 +27,28 @@ for mon in data:
 for index, ty in enumerate(type):
     print(index, ":", ty['english'])
 
-
 ty = input("what type of pokemon do you want?")
-same = True
+""" same = True
 for i in type:
     if i['english'] == ty:
         same = True
     else:
         same = False
-for i in data:
-    if same == True:
-        print(data['name']['english'])
 
+
+    while same == True:
+            print(mon['name']['english']) """
+
+same = True
+Highest_HP = []
+for mons in data:
+    if mons['type'] == ty:
+        same = True
+        while same == True:
+            Highest_HP.append(mons['name']['english'])
+    else:
+        same = False
+    print(Highest_HP)
 #Develop a function to find all pokemon matching the name the user searched for. Ex. if "Char" return Charmander, Charmeleon and Charizard. Make the user aware if no pokemon was found. 
 
 #For Leo/, help me come up with a clever final question, considering maybe showing all moves a pokemon has avaiable based on type
