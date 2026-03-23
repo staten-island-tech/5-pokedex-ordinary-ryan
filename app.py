f9fe5096b0dd5b21fd5ba8648c1dd49b3ce8aea8
ordinary-ryan <ryanl422@nycstudents.net>
2026-03-23 09:41:21 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,17 +2,17 @@ import json
 ## Open the JSON file of pokemon data
 pokedex = open("./pokedex.json", encoding="utf8")
 ty_list = open("./types.json", encoding="utf8")
-
+moves = open("./moves.json", encoding="utf8")
 ## create variable "data" that represents the enitre pokedex list
 data = json.load(pokedex)
 type = json.load(ty_list)
-
+mo = json.load(moves)
 
 # Create a function that will take the data from the JSON file and you will iterate through the list of pokemon and print each pokemons name.
-""" for index, mon in enumerate(data):
-    print(index, ":", mon["name"]['english']) """
+for index, mon in enumerate(data):
+    print(index, ":", mon["name"]['english'])
 # Add a language choice feature and print the pokemons name based on the user input
-""" mon = input("please select a pokemon")
+
 lang = input("Choose a language")
 for mon in data:
     if lang == "japanese":
@@ -21,23 +21,12 @@ for mon in data:
         print(mon['name']['french'])
     elif lang == "chinese":
         print(mon['name']['chinese']) 
- """
+
 """ print(data[0]) """
 # Develop a function that creates a new list of pokemon based on the type the user searched for. If no pokemon was found of that type inform the user
 for index, ty in enumerate(type):
     print(index, ":", ty['english'])
 
-
-""" same = True
-for i in type:
-    if i['english'] == ty:
-        same = True
-    else:
-        same = False
-
-
-    while same == True:
-            print(mon['name']['english']) """
 
 ty = input("what type of pokemon do you want?")
 for mons in data:
@@ -52,6 +41,10 @@ for mon in data:
    
 #For Leo/, help me come up with a clever final question, considering maybe showing all moves a pokemon has avaiable based on type
 
+moves = input("what moves do you want to search for. input a type")
+for poke in mo:
+    if moves == poke['type']:
+        print(poke['ename'])
 """ Highest_HP = 0
 for mon in pokemon:
     if mon['base']['HP'] > Highest_HP:
